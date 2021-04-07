9693a342ff387f4e69e3897d1c33f76d6d1d6885
Nguyen-Ngoc-Hung <ngochung732001@gmail.com>
2021-04-07 17:06:15 +0700

    nothing

--- main.js
+++ main.js
@@ -5,5 +5,7 @@ var canvas = document.getElementById("game-zone");
 var context = canvas.getContext("2d");
 
 var base = new Image();
-base.src="images/base.png";
-context.drawImage(base,100,0);
+base.src="./images/base.png";
+base.onload = function(){
+    context.drawImage(base,100,0);
+    }
